1b602d91be105f88d8b1018417db135139e37cef
Naailah <naailahb.browne@gmail.com>
2023-10-30 23:43:04 +0200

    IT WORKS

--- etch.js
+++ etch.js
@@ -1,6 +1,7 @@
-const container = document.querySelector('#container'); 
+const container = document.querySelector('#container');
 let rows = document.getElementsByClassName("row");
 let counter = 0;
+let loopy = 0;
 
 
 function createGrid() {
@@ -19,23 +20,24 @@ function makeColumns(cInput) {
     for (j = 0; j < rows.length; j++) {
         for (x = 0; x < cInput; x++) {
             let column = document.createElement('div');
-            column.textContent = "m";
-            counter++;
-            //rows[x].appendChild(column).className = "column";
+
             rows[x].appendChild(column).id = counter;
+            document.getElementById(counter).className = 'column';
+            counter++;
         }
     }
-    console.log(document.getElementById(c))
 }
 
+
 function colorTrail(row, column) {
-    for (c = 0; c < 256; c++) {
-        let colorless = document.getElementById(c); 
-        colorless.style.backgroundColor = 'pink';
-        
+    for (p = 0; p < row * column; p++) {
+        document.getElementById(p).addEventListener('mouseover', colors);
     }
 }
-    
+
+function colors(event) {
+    this.style.backgroundColor = 'pink';
+}
 
 createGrid();
 colorTrail(16, 16);
