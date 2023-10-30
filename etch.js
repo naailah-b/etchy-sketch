ceb36878e95c54f89d73975ed4f9bba4c22cb4b3
Naailah <naailahb.browne@gmail.com>
2023-10-31 01:01:44 +0200

    idk added

--- etch.js
+++ etch.js
@@ -3,10 +3,10 @@ let rows = document.getElementsByClassName("row");
 let counter = 0;
 let loopy = 0;
 
-
 function createGrid() {
     makeRows(16);
     makeColumns(16);
+    colorTrail(16, 16);
 }
 
 function makeRows(rInput) {
@@ -28,7 +28,6 @@ function makeColumns(cInput) {
     }
 }
 
-
 function colorTrail(row, column) {
     for (p = 0; p < row * column; p++) {
         document.getElementById(p).addEventListener('mouseover', colors);
@@ -36,8 +35,22 @@ function colorTrail(row, column) {
 }
 
 function colors(event) {
-    this.style.backgroundColor = 'pink';
+    this.style.backgroundColor = 'black';
 }
 
 createGrid();
-colorTrail(16, 16);
+//heart();
+
+let ids = ['35', '36', '37', '42', '43', '44', '50', '51', '52', '53', '54', '57', 
+'58', '59', '60', '61', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74',
+'75', '76', '77', '78', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
+'91', '92', '93', '94', '97', '98', '99', '100', '101', '102', '103', '104', '105',
+'106', '107', '108', '109', '110', '114', '115', '116', '117', '118', '119', '120',
+'121', '122', '123', '124', '125', '131', '132', '133', '134', '135', '136', '137',
+'138', '139', '140', '148', '149', '150', '151', '152', '153', '154', '155', '165',
+'166', '167', '168', '169', '170', '182', '183', '184', '185', '199', '200']
+
+ids.forEach(element => {
+    document.getElementById(element).style.backgroundColor = 'pink'
+})
+
