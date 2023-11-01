23b864b271779d52283af03e7a102e29ccb0fb81
naailah browne <naailahb.browne@gmail.com>
2023-11-01 13:52:15 +0200

    i forgot what i did

--- etch.js
+++ etch.js
@@ -7,6 +7,7 @@ let num1, num2;
 document.getElementById('btnPrompt').addEventListener('click', () => {
     num1 = Number(prompt('Enter the number of rows '));
     num2 = Number(prompt('Enter the number of columns '));
+    delGrid();
     createGrid();
 })
 
@@ -14,12 +15,17 @@ function defaultGrid() {
     makeRows(16);
     makeColumns(16);
     colorTrail(16, 16);
+    heart();
 }
 
 function createGrid() {
     makeRows(num1);
     makeColumns(num2);
     colorTrail(num1, num2);
+}
+
+function delGrid() {
+    document.getElementById('container').innerHTML = ''
 }
 
 function makeRows(rInput) {
@@ -52,9 +58,8 @@ function colors(event) {
     this.style.backgroundColor = 'black';
 }
 
-defaultGrid();
-
-let ids = ['35', '36', '37', '42', '43', '44', '50', '51', '52', '53', '54', '57', 
+function heart() {
+    let ids = ['35', '36', '37', '42', '43', '44', '50', '51', '52', '53', '54', '57', 
 '58', '59', '60', '61', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74',
 '75', '76', '77', '78', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
 '91', '92', '93', '94', '97', '98', '99', '100', '101', '102', '103', '104', '105',
@@ -63,7 +68,12 @@ let ids = ['35', '36', '37', '42', '43', '44', '50', '51', '52', '53', '54', '57
 '138', '139', '140', '148', '149', '150', '151', '152', '153', '154', '155', '165',
 '166', '167', '168', '169', '170', '182', '183', '184', '185', '199', '200']
 
-ids.forEach(element => {
-    document.getElementById(element).style.backgroundColor = 'pink'
-})
+    ids.forEach(element => {
+        document.getElementById(element).style.backgroundColor = 'pink'
+    })
+}
+
+defaultGrid();
+
+
 
